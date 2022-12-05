c626fa378456a58adfea1e0168d417491fd68e85
mihkek <mihkek991@gmail.com>
2022-12-05 20:57:44 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <aikidos-game />
+</template>
+
+<script>
+import AikidosGame from "src/components/AikidosGame.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AikidosGame,
+  },
+});
+</script>
